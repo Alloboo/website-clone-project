9347bce900ad782be05720257c6b8a220da22493
부 <hye_0425@naver.com>
2023-08-10 16:37:57 +0900

    4주차 개발 완료

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import MainContent
+from .models import MainContent, Comment
+
+class MainContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'pub_date'] # 출력할 필드 정의
+    search_fields = ['title'] # title 기준으로 검색
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content_list', 'content', 'author', 'create_date', 'modify_date'] # 출력할 필드 정의
+    search_fields = ['author'] # author 기준으로 검색
 
 # Register your models here.
-admin.site.register(MainContent)
+admin.site.register(MainContent, MainContentAdmin)
+admin.site.register(Comment, CommentAdmin)
 
 
